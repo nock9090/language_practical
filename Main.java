47232b8e028933b269738c5cb1f2e58e38038071
nock9090 <hirata@pegigoo.info>
2025-08-12 14:02:22 +0900

    first class

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args){
+		System.out.print(echon);
+		System.out.print(irohani);
+		System.out.print(hoheton);
+		
+		System.out.print(3+5);
+	}
+}
